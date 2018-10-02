409c713036366979011de25574efb4c74ca9f723
杜健 <1035246868@qq.com>
2018-10-03 05:01:25 +0800

    新闻前后端收藏接口实现＋修改前端代码

--- information18/info/moduls/news/views.py
+++ information18/info/moduls/news/views.py
@@ -1,11 +1,13 @@
 from celery import current_app
-from flask import abort,g
+from flask import abort,g, jsonify
 from flask import render_template
+from flask import request
 
 from info.models import User, News, Category
 from info.moduls.news import news_blu
-from info import constants
+from info import constants, db
 from info.utits.common import user_login_data
+from utits.response_code import RET
 
 
 @news_blu.route('/<int:news_id>')
@@ -35,10 +37,59 @@ def news_detail(news_id):
     for news in news_list if news_list else []:
         click_news_list.append(news.to_basic_dict())
 
+
+    # 判断是否收藏该新闻，默认值为 false
+    is_collected = False
+    # 判断用户是否收藏过该新闻
+    if g.user:
+        if news in g.user.collection_news:
+            is_collected = True
+
     data = {
         "news": news.to_dict(),
         "click_news_list": click_news_list,
+        "is_collected": is_collected,
         "user_info": g.user.to_dict() if g.user else None,
     }
     return render_template('news/detail.html',data = data)
 
+@news_blu.route("/news_collect", methods=['POST'])
+@user_login_data
+def news_collect():
+    """新闻收藏"""
+
+    user = g.user
+    json_data = request.json
+    news_id = json_data.get("news_id")
+    action = json_data.get("action")
+
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    if not news_id:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ("collect", "cancel_collect"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="新闻数据不存在")
+
+    if action == "collect":
+        user.collection_news.append(news)
+    else:
+        user.collection_news.remove(news)
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存失败")
+    return jsonify(errno=RET.OK, errmsg="操作成功")
