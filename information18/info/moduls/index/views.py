2c661ae00b4c549da8bb817ed11f8d8d8dbd202b
杜健 <1035246868@qq.com>
2018-10-08 11:09:51 +0800

    新闻列表数据后端实现前端实现分页效果

--- information18/info/moduls/index/views.py
+++ information18/info/moduls/index/views.py
@@ -1,10 +1,85 @@
 import logging
 from flask import current_app, render_template, session, jsonify
+from flask import request
+
 from info.utits.response_code import RET
 from . import index_bp
 from info import redis_store
 from info.models import User, News, Category
 from info import constants
+
+
+@index_bp.route('/news_list')
+def get_news_list():
+    """获取新闻列表数据"""
+    """
+    1.获取参数
+        1.1 分类id: cid,当前页码：page（默认值：第一页）, 每一页多少条数据：per_page（默认值：10条）
+    2.校验参数
+        2.1 非空判断
+        2.2 整型强制类型转换
+    3.逻辑处理
+        3.1 分页查询
+        3.2 对象列表转字典列表
+    4.返回值
+    """
+    # 1.1 分类id: cid,当前页码：page（默认值：第一页）, 每一页多少条数据：per_page（默认值：10条）
+    param_dict = request.args
+    cid = param_dict.get('cid', "1")
+    page = param_dict.get('page', "1")
+    per_page = param_dict.get('per_page', "10")
+
+    # 2.1 非空判断
+    if not cid:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+    #2.2 整型强制类型转换
+    try:
+        cid = int(cid)
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数格式错误")
+
+    """
+        if cid == 1:
+            # 只需要根据新闻创建时间的降序排序
+            paginate = News.query.order_by(News.create_time.desc()).paginate(page, per_page, False)
+        else:
+            # 需要根据新闻创建时间的降序排序和分类id
+            paginate = News.query.filter(News.category_id == cid).order_by(News.create_time.desc()).paginate(page, per_page, False)
+    """
+    # 条件列表
+    filters = []
+    if cid != 1: # 2 3 4 5
+        # == 符号被sqlalchemy底层给重写了__eq__返回的是一个查询条件，而不是Bool值
+        filters.append(News.category_id == cid)
+    # 3.1 分页查询
+    # *filters 解包
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+        # 当前页码所有数据
+        items = paginate.items
+        # 当前页码
+        current_page = paginate.page
+        # 总页码
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询新闻列表数据异常")
+
+    # 3.2 对象列表转字典列表
+    news_dict_list = []
+    for news in items if items else []:
+        news_dict_list.append(news.to_dict())
+
+    # 3.3 组织返回数据
+    data = {
+        "news_list": news_dict_list,
+        "current_page": current_page,
+        "total_page": total_page
+    }
+    return jsonify(errno=RET.OK, errmsg="查询新闻列表数据成功", data=data)
 
 
 #2. 使用蓝图
